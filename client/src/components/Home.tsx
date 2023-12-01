ef6a08e2756ba4ecf3504add2f11630214636969
Adam Robson <adamray312@gmail.com>
2023-12-01 15:19:35 -0800

    add button to landing page to go to intake

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -2,18 +2,22 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div className="border-2 border-black flex flex-col justify-start items-center w-screen h-screen mx-auto">
-      <div className="p-8 text-center underline underline-offset-4 decoration-2 text-xl/6 md:text-2xl/7 lg:text-3xl/8 tracking-tighter font-semibold subpixel-antialiased">
+    <div className="max-w-2xl border-2 border-black flex flex-col justify-start items-center w-screen h-screen mx-auto p-8">
+      <div className="py-8 text-center underline underline-offset-4 decoration-2 text-5xl/8 font-light tracking-tighter subpixel-antialiased">
         Resumate
       </div>
 
-      <div className="w-full h-96 p-4 mx-auto max-w-3xl flex flex-col justify-between items-center">
+      <div className="w-full h-96 mx-auto flex flex-col justify-between items-center px-10">
         <p className="my-4 text-center text-base/4 sm:text-lg/5 md:text-xl/6 lg:text-2xl/7 tracking-tighter font-light subpixel-antialiased">Welcome to Resumate!</p>
-        <p className="my-4 p-4 text-base/4 sm:text-lg/5 md:text-xl/6 lg:text-2xl/7 tracking-tighter font-light subpixel-antialiased">Generate content that is customized for the role to which you are applying.</p>
-        <p className="max-w-xl w-full mx-auto text-sm/4 sm:text-base/5 md:text-lg/6 lg:text-xl/7 tracking-tighter font-light subpixel-antialiased">The button below will take you to a form where you will enter information. Resumate will generate the content based on your personal information.</p>
+        <p className="text-center text-base/4 sm:text-lg/5 md:text-xl/6 lg:text-2xl/7 tracking-tighter font-light subpixel-antialiased">Generate content that is customized for the role to which you are applying.</p>
+        <p className="text-center text-base/4 sm:text-lg/5 md:text-xl/6 lg:text-2xl/7 tracking-tighter font-light subpixel-antialiased">The button below will take you to a form where you will enter information. Resumate will generate the content based on your personal information.</p>
       </div>
-      <button>
-        <Link to="/form/personal" className="">Go to Form</Link>
+      <button className="my-6">
+        <Link to="/form/personal">Enter Form</Link>
+      </button>
+
+      <button className="my-6">
+        <Link to="/intake">Paste Job</Link>
       </button>
     </div>
   );
